1e4089b098f4f251f710d3981e8c19ad32e7e82e
FranciscoPC <fpcastro46@gmail.com>
2026-02-06 12:29:51 +0100

    Añadido proyecto explicativo sobre Programación Orienta a Objetos en Java.

---
+++ Ejemplo_POO/src/daw/cotarelo/fpc/principal/Main.java
@@ -0,0 +1,16 @@
+package daw.cotarelo.fpc.principal;
+
+/**
+ *
+ * @author fperez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
