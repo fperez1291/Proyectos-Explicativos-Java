ef933761c8eae3e359556846668bac115face4d9
Francisco Pérez Castro <fpcastro46@gmail.com>
2026-02-07 21:03:09 +0100

    Modificado el código para explicar y ejemplificar el uso de los constructores de forma más completa y precisa, además de sencilla.

--- Ejemplo_StringBuilder/src/daw/cotarelo/fpc/principal/Main.java
+++ Ejemplo_StringBuilder/src/daw/cotarelo/fpc/principal/Main.java
@@ -6,7 +6,6 @@ package daw.cotarelo.fpc.principal;
  * clase String, los constructores para StringBuilder y los métodos que debe
  * conocer.
  *
- *
  * @author fperez
  */
 public class Main {
@@ -17,36 +16,41 @@ public class Main {
     public static void main(String[] args) {
 
         /**
-         * ====================== ¿Qué es StringBuilder? ======================
-         * La clase StringBuilder se corresponde con lo que llamamos
-         * "constructor de Strings". Se puede usar en lugar de la clase String
-         * en aquellos casos en los que deseamos construir un String para
-         * morstrar por pantalla (en la salida del programa) o bien para cuando
-         * se modificará frecuentemente una cadena de caracteres. 
-         * =====
+         * Constructor vacío: construye un objeto de la clase StringBuilder sin
+         * caracteres y con una capacidad inicial de 16 caracteres. <br>
+         * [SINTAXIS] StringBuilder nameObject = new StringBuilder();
          */
-        /**
-         * ================== Constructores de StringBuilder ==================
-         *
-         * Construimos un objeto (str) de la clase StringBuilder. Para ello,
-         * contamos con 3 constructores:
-         *
-         * 1) StringBuilder() 2) StringBuilder(int capacidad) 3)
-         * StringBuilder(String str)
-         *
-         * Tenga en cuenta que los constructores numerados no son la forma
-         * concreta de usar el constructor, sino su cabecera (en términos más
-         * comprensibles, su sintaxis junto con la lista de parámetros
-         * (separados por comas) con su tipo y un nombre generico para que se
-         * entienda exactamente qué espera el constructor en ese parámetro).
-         */
-        // 1) Objeto de la clase StringBuilder vacío con capacidad máxima de 16 caracteres
         StringBuilder str1 = new StringBuilder();
-        // 2) Objeto de la clase StringBuilder vacío con capacidad máxima de 100 caracteres
-        StringBuilder str2 = new StringBuilder(100);
-        // 3) StringBuilder sin caracteres almacenados y con capacidad inicial de 16 caracteres
-        StringBuilder str3 = new StringBuilder("Hola Mundo");
 
+        /**
+         * Constructor con capacidad inicial: construye un StringBuilder sin
+         * caracteres y con una capacidad inicial especificada en el arguemnto
+         * capacidad. <br>
+         * [SINTAXIS] StringBuilder nameObject = new StringBuilder(capacidad);
+         * <br>
+         * capacidad: puede ser o un literal de tipo int o el nombre de una
+         * variable de tipo int
+         */
+        StringBuilder str2 = new StringBuilder(10);
+
+        int capacidad = 20;
+        StringBuilder str3 = new StringBuilder(capacidad);
+
+        /**
+         * Constructor con String inicial: construye un StringBuilder
+         * inicializado con el contenido de la cadena pasada por parámetro. La
+         * capacidad inicial del StringBuilder es de 16 más la longitud de la
+         * cadena pasada por parámetro. <br>
+         * [SINTAXIS] StringBuilder nombreObjeto = new StringBuilder(cadena);
+         * <br>
+         * cadena: puede ser una cadena de texto entre comillas o el nombre de
+         * un objeto de la clase String
+         */
+        StringBuilder str4 = new StringBuilder("Hola Mundo");
+        
+        String cadena = "Programacion en Java";
+        StringBuilder str5 = new StringBuilder(cadena);
+        
         /**
          * ======================== Métodos a conocer ========================
          *
