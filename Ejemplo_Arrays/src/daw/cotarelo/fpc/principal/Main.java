4113e7959737af9389b33f85a91f603040c02b90
Francisco Pérez Castro <fpcastro46@gmail.com>
2026-02-08 21:07:13 +0100

    Añadida la declaración de 3 arrays: numeros (array de int), cadenas (array de String) y validez (array de boolean).

--- Ejemplo_Arrays/src/daw/cotarelo/fpc/principal/Main.java
+++ Ejemplo_Arrays/src/daw/cotarelo/fpc/principal/Main.java
@@ -10,6 +10,15 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int[] numeros = new int[5];
+        
+        
+        String cadenas[];
+        cadenas = new String[5];
+        
+        
+        boolean[] validez = {true, true, false, true, false};
+        
         
     }
     
